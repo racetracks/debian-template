5af461a56ecc9e55fc15139a967cb18f1d721d75
racetracks <95535303+racetracks@users.noreply.github.com>
2023-12-11 22:17:41 +1030

    Update 00-bootstrap.sh

--- 00-bootstrap.sh
+++ 00-bootstrap.sh
@@ -1,7 +1,34 @@
 #!/bin/bash
 
 # Update APT cache
-sudo apt update
+apt update
 
 # Install Ansible
-sudo apt install -y ansible
+apt install -y ansible sudo
+
+#!/bin/bash
+
+# Check if the script is run as root
+if [ "$EUID" -ne 0 ]; then
+    echo "Please run as root."
+    exit 1
+fi
+
+# Install sudo
+
+
+
+# Add a user to the sudo group (replace 'dc' with the desired username)
+usermod -aG sudo dc
+
+# Disable su
+passwd -l root
+
+# Disable root from SSH login
+echo "PermitRootLogin no" >> /etc/ssh/sshd_config
+
+# Restart SSH service to apply changes
+service ssh restart
+
+echo "Setup complete. Make sure to test the SSH login with the user account before logging out as root."
+
