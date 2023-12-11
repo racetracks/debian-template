ff54993458b00999c81facde7272f5f23ce74df2
racetracks <95535303+racetracks@users.noreply.github.com>
2023-12-11 22:38:56 +1030

    Create 01-configure-netplan.sh

---
+++ 01-configure-netplan.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Check if the script is run as root
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run as root"
+  exit
+fi
+
+# Install netplan if not already installed
+apt update
+apt install -y netplan.io
+
+# Disable the existing networking stack
+systemctl stop networking
+systemctl disable networking
+systemctl mask networking
+
+# Detect the primary Ethernet interface dynamically
+eth_interface=$(ip -o -4 route show to default | awk '{print $5}')
+
+# Create a Netplan YAML configuration file
+cat <<EOL > /etc/netplan/01-netcfg.yaml
+network:
+  version: 2
+  renderer: networkd
+  ethernets:
+    $eth_interface:
+      dhcp4: true
+EOL
+
+# Apply the Netplan configuration
+netplan apply
+
+echo "Netplan configuration applied successfully for interface: $eth_interface."
