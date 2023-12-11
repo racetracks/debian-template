e5a921300d25aae08bb4997cbacd0aecf1491028
racetracks <95535303+racetracks@users.noreply.github.com>
2023-12-11 22:22:56 +1030

    Update 00-bootstrap.sh

--- 00-bootstrap.sh
+++ 00-bootstrap.sh
@@ -50,3 +50,39 @@ echo "net.ipv6.conf.default.disable_ipv6 = 1" >> /etc/sysctl.conf
 sysctl -p
 
 echo "IPv6 has been disabled. Please restart the network for the changes to take effect."
+
+#!/bin/bash
+
+# Check if the script is run as root
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run as root"
+  exit
+fi
+
+# Install netplan if not already installed
+apt update
+apt install -y netplan.io
+
+# Disable the existing networking stack
+systemctl stop networking
+systemctl disable networking
+systemctl mask networking
+
+# Detect the primary Ethernet interface dynamically
+eth_interface=$(ip -o -4 route show to default | awk '{print $5}')
+
+# Create a Netplan YAML configuration file
+cat <<EOL > /etc/netplan/01-netcfg.yaml
+network:
+  version: 2
+  renderer: networkd
+  ethernets:
+    $eth_interface:
+      dhcp4: true
+EOL
+
+# Apply the Netplan configuration
+netplan apply
+
+echo "Netplan configuration applied successfully for interface: $eth_interface."
+
