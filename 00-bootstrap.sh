8182a1ebaa6785e8bde4ccf4483f639b141ac0f3
racetracks <95535303+racetracks@users.noreply.github.com>
2023-12-11 22:19:30 +1030

    Update 00-bootstrap.sh

--- 00-bootstrap.sh
+++ 00-bootstrap.sh
@@ -32,3 +32,21 @@ service ssh restart
 
 echo "Setup complete. Make sure to test the SSH login with the user account before logging out as root."
 
+
+
+#!/bin/bash
+
+# Check if the script is run as root
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root. Exiting."
+   exit 1
+fi
+
+# Disable IPv6
+echo "net.ipv6.conf.all.disable_ipv6 = 1" >> /etc/sysctl.conf
+echo "net.ipv6.conf.default.disable_ipv6 = 1" >> /etc/sysctl.conf
+
+# Apply the changes
+sysctl -p
+
+echo "IPv6 has been disabled. Please restart the network for the changes to take effect."
