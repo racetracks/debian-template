a3a09e002d35c29c72947158fbdb405e2f524bee
dan <dan@axfr.cc>
2024-09-21 03:25:52 +0930

    remove deprecated setup files

--- 04-allow-sshd-inbound.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo iptables -A INPUT -p tcp -s 10.100.100.0/24 --dport 22 -j ACCEPT
-sudo iptables -A INPUT -m conntrack --ctstate ESTABLISHED -j ACCEPT
-sudo iptables -A INPUT -j DROP
-sudo iptables-save | sudo tee /etc/iptables/rules.v4
-sudo iptables -L -n -v
